746c333d292a0e0923457ef80490956d02a026e6
Benjamin Davies <benjamindavies@mmc.school.nz>
2019-07-29 15:07:53 +1200

    Start implementing score saving

---
+++ DodgeGame/Scoreboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DodgeGame
+{
+    static class Scoreboard
+    {
+        private static HttpClient httpClient;
+        public static void PostScore(string name, int lifeCount, int score)
+        {
+            Task.Run(async () =>
+            {
+                if (httpClient == null)
+                {
+                    httpClient = new HttpClient();
+                }
+
+                await httpClient.PostAsync("http://php.mmc.school.nz/");
+            });
+        }
+    }
+}
