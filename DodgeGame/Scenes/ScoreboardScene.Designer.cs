59ea867d7f18cb0e584b99f7f45ab5e60087afae
Benjamin-Davies <bentendavies@gmail.com>
2019-07-31 20:59:47 +1200

    More progress on implementing scoreboard

--- DodgeGame/Scenes/ScoreboardScene.Designer.cs
+++ DodgeGame/Scenes/ScoreboardScene.Designer.cs
@@ -31,10 +31,10 @@
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.titleLable = new System.Windows.Forms.Label();
             this.BackButton = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.scoreTable = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.scoreTable.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel
@@ -45,7 +45,7 @@
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel.Controls.Add(this.titleLable, 1, 0);
             this.tableLayoutPanel.Controls.Add(this.BackButton, 1, 2);
-            this.tableLayoutPanel.Controls.Add(this.tableLayoutPanel1, 1, 1);
+            this.tableLayoutPanel.Controls.Add(this.scoreTable, 1, 1);
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(14, 13, 14, 13);
@@ -73,6 +73,7 @@
             // 
             // BackButton
             // 
+            this.BackButton.Click += new System.EventHandler(this.ScoreboardScene_Click);
             this.BackButton.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BackButton.Location = new System.Drawing.Point(138, 315);
             this.BackButton.Name = "BackButton";
@@ -81,21 +82,22 @@
             this.BackButton.Text = "Back";
             this.BackButton.UseVisualStyleBackColor = true;
             // 
-            // tableLayoutPanel1
+            // scoreTable
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(138, 147);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(400, 162);
-            this.tableLayoutPanel1.TabIndex = 5;
+            this.scoreTable.ColumnCount = 3;
+            this.scoreTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.scoreTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.scoreTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.scoreTable.Controls.Add(this.label1, 0, 0);
+            this.scoreTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scoreTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreTable.Location = new System.Drawing.Point(138, 147);
+            this.scoreTable.Name = "scoreTable";
+            this.scoreTable.RowCount = 2;
+            this.scoreTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.scoreTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.scoreTable.Size = new System.Drawing.Size(400, 162);
+            this.scoreTable.TabIndex = 5;
             // 
             // label1
             // 
@@ -111,13 +113,14 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "ScoreboardScene";
             this.Size = new System.Drawing.Size(678, 480);
             this.Load += new System.EventHandler(this.ScoreboardScene_Load);
             this.tableLayoutPanel.ResumeLayout(false);
             this.tableLayoutPanel.PerformLayout();
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
+            this.scoreTable.ResumeLayout(false);
+            this.scoreTable.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -127,7 +130,7 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
         private System.Windows.Forms.Label titleLable;
         private System.Windows.Forms.Button BackButton;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel scoreTable;
         private System.Windows.Forms.Label label1;
     }
 }
--- DodgeGame/Scenes/ScoreboardScene.cs
+++ DodgeGame/Scenes/ScoreboardScene.cs
@@ -1,34 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DodgeGame.Properties;
 
 namespace DodgeGame.Scenes
 {
     public partial class ScoreboardScene : UserControl, IScene
     {
-        public ScoreboardScene()
+        private MainWindow form;
+        private Navigator navigator;
+        private CancellationTokenSource loading;
+
+        public ScoreboardScene(MainWindow _form, Navigator _navigator)
         {
             InitializeComponent();
+
+            BackButton.BackColor = Resources.AltBackColor;
+
+            form = _form;
+            navigator = _navigator;
         }
 
         public void Resume()
         {
-            // TODO: load scores
+            loading = new CancellationTokenSource();
+            Task.Run(async () =>
+            {
+                var scores = await Scoreboard.GetScores();
+
+                Invoke((Action)(() =>
+                {
+                    scoreTable.SuspendLayout();
+
+                    for (int i = scoreTable.RowCount - 1; i >= 1; i--)
+                    {
+                        scoreTable.RowStyles.RemoveAt(i);
+                    }
+
+                    scoreTable.RowCount = scores.Count + 2;
+                    int j = 1;
+                    foreach (var score in scores)
+                    {
+                        scoreTable.RowStyles.Add(
+                            new RowStyle(SizeType.Absolute, 20));
+
+                        var usernameLabel = new Label
+                        {
+                            AutoSize = true,
+                            Text = score.Username
+                        };
+                        scoreTable.Controls.Add(usernameLabel, 0, j);
+
+                        j++;
+                    }
+                    scoreTable.RowStyles.Add(new RowStyle(SizeType.Absolute, 0));
+
+                    scoreTable.ResumeLayout();
+                }));
+
+                loading = null;
+            }, loading.Token);
+        }
+
+        public void Pause()
+        {
+            loading?.Cancel();
         }
 
         // Unused interface methods
-        public void Pause() { }
         public void UpdateScene() { }
 
         private void ScoreboardScene_Load(object sender, EventArgs e)
         {
             Dock = DockStyle.Fill;
+        }
+
+        private void ScoreboardScene_Click(object sender, EventArgs e)
+        {
+            navigator.Pop();
         }
     }
 }
