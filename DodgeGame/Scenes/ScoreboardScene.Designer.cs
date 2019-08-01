351ba73a49b80b5ba4d3076447454c51479f037d
Benjamin Davies <benjamindavies@mmc.school.nz>
2019-08-01 12:30:08 +1200

    Finish scoreboard

--- DodgeGame/Scenes/ScoreboardScene.Designer.cs
+++ DodgeGame/Scenes/ScoreboardScene.Designer.cs
@@ -32,7 +32,9 @@
             this.titleLable = new System.Windows.Forms.Label();
             this.BackButton = new System.Windows.Forms.Button();
             this.scoreTable = new System.Windows.Forms.TableLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.usernameTitleLabel = new System.Windows.Forms.Label();
+            this.livesTitleLabel = new System.Windows.Forms.Label();
+            this.scoreTitleLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel.SuspendLayout();
             this.scoreTable.SuspendLayout();
             this.SuspendLayout();
@@ -73,7 +75,6 @@
             // 
             // BackButton
             // 
-            this.BackButton.Click += new System.EventHandler(this.ScoreboardScene_Click);
             this.BackButton.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BackButton.Location = new System.Drawing.Point(138, 315);
             this.BackButton.Name = "BackButton";
@@ -81,36 +82,61 @@
             this.BackButton.TabIndex = 4;
             this.BackButton.Text = "Back";
             this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.ScoreboardScene_Click);
             // 
             // scoreTable
             // 
+            this.scoreTable.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
             this.scoreTable.ColumnCount = 3;
             this.scoreTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.scoreTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.scoreTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.scoreTable.Controls.Add(this.label1, 0, 0);
+            this.scoreTable.Controls.Add(this.usernameTitleLabel, 0, 0);
+            this.scoreTable.Controls.Add(this.livesTitleLabel, 1, 0);
+            this.scoreTable.Controls.Add(this.scoreTitleLabel, 2, 0);
             this.scoreTable.Dock = System.Windows.Forms.DockStyle.Fill;
             this.scoreTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.scoreTable.Location = new System.Drawing.Point(138, 147);
             this.scoreTable.Name = "scoreTable";
             this.scoreTable.RowCount = 2;
-            this.scoreTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.scoreTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.scoreTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.scoreTable.Size = new System.Drawing.Size(400, 162);
             this.scoreTable.TabIndex = 5;
             // 
-            // label1
+            // usernameTitleLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.usernameTitleLabel.AutoSize = true;
+            this.usernameTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTitleLabel.Location = new System.Drawing.Point(4, 1);
+            this.usernameTitleLabel.Name = "usernameTitleLabel";
+            this.usernameTitleLabel.Size = new System.Drawing.Size(132, 29);
+            this.usernameTitleLabel.TabIndex = 0;
+            this.usernameTitleLabel.Text = "Username";
+            // 
+            // livesTitleLabel
+            // 
+            this.livesTitleLabel.AutoSize = true;
+            this.livesTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.livesTitleLabel.Location = new System.Drawing.Point(203, 1);
+            this.livesTitleLabel.Name = "livesTitleLabel";
+            this.livesTitleLabel.Size = new System.Drawing.Size(61, 30);
+            this.livesTitleLabel.TabIndex = 1;
+            this.livesTitleLabel.Text = "Lives";
+            // 
+            // scoreTitleLabel
+            // 
+            this.scoreTitleLabel.AutoSize = true;
+            this.scoreTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreTitleLabel.Location = new System.Drawing.Point(283, 1);
+            this.scoreTitleLabel.Name = "scoreTitleLabel";
+            this.scoreTitleLabel.Size = new System.Drawing.Size(82, 29);
+            this.scoreTitleLabel.TabIndex = 2;
+            this.scoreTitleLabel.Text = "Score";
             // 
             // ScoreboardScene
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 42F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -131,6 +157,8 @@
         private System.Windows.Forms.Label titleLable;
         private System.Windows.Forms.Button BackButton;
         private System.Windows.Forms.TableLayoutPanel scoreTable;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label usernameTitleLabel;
+        private System.Windows.Forms.Label livesTitleLabel;
+        private System.Windows.Forms.Label scoreTitleLabel;
     }
 }
--- DodgeGame/Scenes/ScoreboardScene.cs
+++ DodgeGame/Scenes/ScoreboardScene.cs
@@ -24,27 +24,45 @@ namespace DodgeGame.Scenes
 
         public void Resume()
         {
+            // Create an object so that loading the scores can be cancelled if we leave the scene
             loading = new CancellationTokenSource();
+
+            // Load the scores asynchronously so that the UI can still be responsive
             Task.Run(async () =>
             {
+                // Load the scores list
                 var scores = await Scoreboard.GetScores();
 
+                // Make sure that we update the UI on the UI thread
                 Invoke((Action)(() =>
                 {
+                    // Suspend the layout cycle for the table
+                    // so that it doesn't update while we are changing it
                     scoreTable.SuspendLayout();
 
+                    // Delete all of the rows, except for the first one
                     for (int i = scoreTable.RowCount - 1; i >= 1; i--)
                     {
                         scoreTable.RowStyles.RemoveAt(i);
                     }
+                    // Delete all of the labels, except for the first row (3)
+                    for (int i = scoreTable.Controls.Count - 1; i >= 3; i--)
+                    {
+                        scoreTable.Controls.RemoveAt(i);
+                    }
 
-                    scoreTable.RowCount = scores.Count + 2;
+                    // Set the correct number of rows
+                    scoreTable.RowCount = scores.Length + 2;
+                    // Create a counter so that we can set the correct row
                     int j = 1;
+                    // Loop through the scores
                     foreach (var score in scores)
                     {
+                        // Create a row for each one
                         scoreTable.RowStyles.Add(
-                            new RowStyle(SizeType.Absolute, 20));
+                            new RowStyle(SizeType.Absolute, 30));
 
+                        // Create the labels and add them to the table
                         var usernameLabel = new Label
                         {
                             AutoSize = true,
@@ -52,19 +70,39 @@ namespace DodgeGame.Scenes
                         };
                         scoreTable.Controls.Add(usernameLabel, 0, j);
 
+                        var lifeCountLabel = new Label
+                        {
+                            AutoSize = true,
+                            Text = score.LifeCount.ToString()
+                        };
+                        scoreTable.Controls.Add(lifeCountLabel, 1, j);
+
+                        var scoreLabel = new Label
+                        {
+                            AutoSize = true,
+                            Text = score.Score.ToString()
+                        };
+                        scoreTable.Controls.Add(scoreLabel, 2, j);
+
+                        // Increment our row counter
                         j++;
                     }
+
+                    // Add an extra row so that it works properly
                     scoreTable.RowStyles.Add(new RowStyle(SizeType.Absolute, 0));
 
+                    // Start the layout again
                     scoreTable.ResumeLayout();
                 }));
 
+                // Get rid of the cancelation token because we don't need it anymore
                 loading = null;
             }, loading.Token);
         }
 
         public void Pause()
         {
+            // Cancel loading, if it exists
             loading?.Cancel();
         }
 
