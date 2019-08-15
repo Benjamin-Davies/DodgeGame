885cb370cbafa24bd9686a9a8e00e21bb73c7022
Benjamin Davies <benjamindavies@mmc.school.nz>
2019-08-15 12:30:28 +1200

    Add class for RapidApi

---
+++ DodgeGame/RapidApi.cs
@@ -0,0 +1,15 @@
+﻿using DodgeGame.Properties;
+using System.Threading.Tasks;
+
+namespace DodgeGame
+{
+    static class RapidApi
+    {
+        private static readonly string ApiKey = Resources.RapidApiKey;
+
+        public static async Task<bool> BadWordFilter(string text)
+        {
+            return false;
+        }
+    }
+}
